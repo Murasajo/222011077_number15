3db9c0c2938701a1003767a9d44a7eed2c3e61a6
Murasa Joseph <120396358+Murasajo@users.noreply.github.com>
2023-11-02 23:42:46 +0200

    Add files via upload

---
+++ Assign2.java
@@ -0,0 +1,37 @@
+public class Assign2 {
+    int P;
+    int t;
+    int n;
+    double r;
+
+    public Assign2(int P, int t, int n, double r) {
+
+        // Initializing the variables in Assign2
+        this.P = P;
+        this.t = t;
+        this.n = n;
+        this.r = r;
+    }
+
+    public double calculateTotalAmountPaid() {
+        int totalCompoundingPeriods = t * n;
+        double ratePerPeriod = r / n;
+
+        // Calculate the total amount paid using the amount formula on compund interest
+        double amountPaid = P * Math.pow(1 + ratePerPeriod, totalCompoundingPeriods); 
+        return amountPaid;
+    }
+
+    public static void main(String[] args) {
+        int principal = 500000;
+        double annualRate = 0.18;
+        int years = 3;
+        int compoundingPeriodsPerYear = 12;
+
+        // Create an instance of Assign2 with the provided values
+        Assign2 myAmount = new Assign2(principal, years, compoundingPeriodsPerYear, annualRate);
+        double totalAmountPaid = myAmount.calculateTotalAmountPaid(); //calling an instance/object to calculate total amount paid
+
+        System.out.println("Mr.Bob will have paid " + totalAmountPaid + " rwf, after 3 years");
+    }
+}
