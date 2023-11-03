2cb5b73a8d07a9f03b6e8bb7f30a3bcc8e7c04c5
Murasa Joseph <120396358+Murasajo@users.noreply.github.com>
2023-11-03 10:26:31 +0200

    Add files via upload

--- Assign1.java
+++ Assign1.java
@@ -1,14 +1,17 @@
 public class Assign1 {
+    // Method to calculate simple interest
     public static int Sinterest (int Pr, int T, int R ){
         return (Pr*T*R)/100; 
     }
 
 
         public static void main(String[] args){
+            // Creating an instance of the Assign1 class
             Assign1 Mysimpleinterest = new Assign1();
-
+            
             int result = Mysimpleinterest.Sinterest(100000,5,5);
 
+            // Displaying the result
             System.out.println("My simple interest: " + result);
         }
 
---
+++ Unary3.java
@@ -0,0 +1,14 @@
+public class Unary3 {
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = -10;
+        boolean c= true;
+        boolean d= false;
+        
+        System.out.println(~a);//-11
+        System.out.println(~b);//9
+        System.out.println(!c);//false
+        System.out.println(!d);//true
+    }
+}
