af2f1b4c9fa8b74f813e4cb08f4a47cbe457b59f
Joseph Murasa DUSHIMIMANA <120396358+Murasajo@users.noreply.github.com>
2023-11-04 09:04:37 +0200

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,4 +1,4 @@
-public class Assign1 {
+public class SimpleInterest {
     // Method to calculate simple interest
     public static int Sinterest (int Pr, int T, int R ){
         return (Pr*T*R)/100; 
@@ -7,9 +7,9 @@ public class Assign1 {
 
         public static void main(String[] args){
             // Creating an instance of the Assign1 class
-            Assign1 Mysimpleinterest = new Assign1();
+            SimpleInterest MysimpleInter = new SimpleInterest();
             
-            int result = Mysimpleinterest.Sinterest(100000,5,5);
+            int result = MysimpleInter.Sinterest(100000,5,5);
 
             // Displaying the result
             System.out.println("My simple interest: " + result);
